95c515669502c8c0b0acd772353a0d9d132497b2
theofylaktos <theofylaktos.a@gmail.com>
2020-02-27 15:14:23 +0200

    Updates

---
+++ precache-manifest.fd7358fd6aafc4fbf06740fed795a7c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "264b9924f3d1ba5cbe60aeacafee8aac",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "02a02a1916fee0d2e973",
+    "url": "/monsters-rolodex/static/css/main.abeda6f9.chunk.css"
+  },
+  {
+    "revision": "9ea52c5ec0fe8f2ca6dc",
+    "url": "/monsters-rolodex/static/js/2.bb07903f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.bb07903f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02a02a1916fee0d2e973",
+    "url": "/monsters-rolodex/static/js/main.a864a4aa.chunk.js"
+  },
+  {
+    "revision": "24034436bd08b53a5646",
+    "url": "/monsters-rolodex/static/js/runtime-main.6386504b.js"
+  }
+]);
